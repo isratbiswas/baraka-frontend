34fb27cec978a0bb2067b7a80aa043ae7de9eaf5
isratbiswas <isratbiswas28@gmail.com>
2026-01-10 20:30:56 +0600

    project setup

---
+++ src/compontents/Navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <header>
+      <div className="container flex items-center justify-between">
+        <div className="">
+          <h4>BARAKA-E-Shop</h4>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
